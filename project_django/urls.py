1bbd997228547b78980c2819d25018cc3c3ce3bd
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2022-10-30 14:42:09 +0700

    Submission done and Authors on progress

--- project_django/urls.py
+++ project_django/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('submission/', include('submission.urls')),
+    path('authors/', include('authors.urls')),
 ]
---
+++ submission/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from submission.models import Track, Submission
+from  .filters import SubmissionFilter
+from django.http import HttpResponse
+from django.core import serializers
+
+def show_submission(request):
+    track_data = Track.objects.all()
+    submission_data = Submission.objects.all()
+    submission_filter = SubmissionFilter(request.GET, queryset=submission_data)
+    context = {'track' : track_data,
+                'submission_filter' : submission_filter}
+    return render(request, "submission.html", context)
+
+def show_details_by_id(request, id):
+    track_data = Track.objects.filter(id=id)
+    submission_data = Submission.objects.filter(id=id)
+    context = {'track' : track_data,
+                'submission' : submission_data}
+    return render(request, "details.html", context)
+
+def show_json(request):
+    submission_data = Submission.objects.all()
+    submission_filter = SubmissionFilter(request.GET, queryset=submission_data)
+    return HttpResponse(serializers.serialize("json", submission_filter.qs), content_type="application/json")
