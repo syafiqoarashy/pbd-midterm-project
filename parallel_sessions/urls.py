7e72502d65bc0bffd0a924fda4a9a620c8f3a487
unknown <matthewrizkyhartadi@gmail.com>
2022-11-02 21:53:32 +0700

    finish parallel sessions

--- parallel_sessions/urls.py
+++ parallel_sessions/urls.py
@@ -1,21 +1,10 @@
 from django.urls import path
-from .views import show_sessions, category, AME, BBE, CPE, ECE, IE, ISBE, IT, MME, SBCC, SCE, show_json_category
+from .views import category, show_json_category
 
 app_name = 'parallel_sessions'
 
 urlpatterns = [
     path('', category, name='category'),
-    path('category_sessions', show_sessions, name='category_sessions'),
-    path('AME', AME, name="AME"),
-    path('BBE', BBE, name="BBE"),
-    path('CPE', CPE, name="CPE"),
-    path('ECE', ECE, name="ECE"),
-    path('IE', IE, name="IE"),
-    path('ISBE', ISBE, name="ISBE"),
-    path('IT', IT, name="IT"),
-    path('MME', MME, name="MME"),
-    path('SBCC', SBCC, name="SBCC"),
-    path('SCE', SCE, name="SCE"),
     path('json_category/', show_json_category, name="json_category"),
     
 ]
