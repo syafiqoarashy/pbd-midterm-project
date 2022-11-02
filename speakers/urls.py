f922233ccd95279d301bbf0cbdcdccbd36f66ec7
Azra Batrisyia Hisman <azrahisman@gmail.com>
2022-11-02 20:24:37 +0700

    ajax

--- speakers/urls.py
+++ speakers/urls.py
@@ -6,6 +6,6 @@ app_name = 'speakers'
 urlpatterns = [
     path('', show_speakers, name='show_speakers'),
     path('show_speakers_info/<int:id>/', show_speakers_info, name='show_speakers_info'),
-    path("search/", SearchResultsView.as_view(), name="search_speakers"),
+    path('search/', SearchResultsView.as_view(), name="search_speakers"),
     path('json/', show_json, name='show_json'),
 ]
