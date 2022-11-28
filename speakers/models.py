400255be1a349c16a7a26576bd0872522c5d00e2
Azra Batrisyia Hisman <azrahisman@gmail.com>
2022-11-28 19:17:20 +0700

    modified models and resolve conflicts

--- speakers/models.py
+++ speakers/models.py
@@ -2,10 +2,6 @@ from dataclasses import Field
 from distutils.command.upload import upload
 from django.db import models
 
-def user_directory_path(filename):
-    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-    return f'speakers/2019/{filename}'
-
 class Speakers(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
@@ -13,7 +9,6 @@ class Speakers(models.Model):
     country = models.CharField(max_length=30)
     type = models.CharField(max_length=10)
     profile = models.TextField()
-    photoFilename = models.ImageField(upload_to=user_directory_path)
     storagePath = models.CharField(max_length=100)
     speechTitle = models.CharField(max_length=100, null=True)
     speechAbstract = models.TextField()
